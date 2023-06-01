77e60f705355982082e39ddf54619f344d5ae616
pxntelle <taniapantelle@gmail.com>
2023-06-01 22:01:09 +0200

    3 task fixed - saveMessage & updateOutput rewritten

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,34 +4,34 @@ const CURRENT_FORM_STATE = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-let formState = JSON.parse(localStorage.getItem(CURRENT_FORM_STATE)) || {};
+let formState = {};
 
 form.addEventListener('input', throttle(onTextInput, 500));
 updateOutput();
 form.addEventListener('submit', saveMessage);
 
 function onTextInput(evt) {
-  formState[evt.target.name] = evt.target.value;
+  formState[evt.target.name] = evt.target.value.trim();
   localStorage.setItem(CURRENT_FORM_STATE, JSON.stringify(formState));
 }
 
 function saveMessage(evt) {
   evt.preventDefault();
-  formState.email = form.elements.email.value;
-  formState.message = form.elements.message.value;
   console.log('formState', formState);
-
-  updateOutput();
   localStorage.removeItem(CURRENT_FORM_STATE);
   formState = {};
-  form.reset();
+  evt.target.reset();
 }
 
 function updateOutput() {
-  const data = JSON.parse(localStorage.getItem(CURRENT_FORM_STATE));
-  if (data) {
-    form.elements.email.value = data.email || '';
-    form.elements.message.value = data.message || '';
+  try {
+    const data = localStorage.getItem(CURRENT_FORM_STATE);
+    if (!data) return;
+    formState = JSON.parse(data);
+    Object.entries(formState).forEach(([key, val]) => {
+      form.elements[key].value = val;
+    });
+  } catch (error) {
+    console.log(error.message);
   }
-  return;
 }
